6c6409e79a0a5f6cf3ba3d21830debb43d174fb9
Santosh Nyaupane <74913954+Dhakeswor17@users.noreply.github.com>
2021-04-20 11:06:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+//let js = "amazing";
+//if (js== "amazing") alert ("JavaScript is fun");
+
+/*console.log(40+90-40);
+let firstName = "Santosh";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myfirstJob = "Programmer";
+let myCurrentJob = "teacher";
+console.log(myfirstJob);
+
+  // Assignment
+
+let country = "Nepal";
+let continent= "Asia";
+let population= "3000000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+ //lecture 2
+
+ let nepal= false;
+ let language;
+ console.log(typeof nepal);
+ console.log(typeof population);
+ console.log(typeof country);
+ console.log(typeof language);
+
+
+ let JavaScriptIsFun = true;
+ console.log(JavaScriptIsFun);
+
+ console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof'Jonna');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+ let year;
+ console.log(year);
+ console.log(typeof year);
+
+ year = 1994;
+ console.log(typeof year);
+
+ console.log(typeof null);
+const now = 2037;
+ const ageJonas = now -1991;
+ const ageSarah = now-2018;
+ console.log(ageJonas, ageSarah);
+ console. log(ageJonas*2, ageSarah/2, 2**3);
+ const firstName = ' Jonas';
+ const lastname = 'Schmedtmann';
+ 
+ let x = 10 + 5;
+ x+=10; // x= X+ 10 = 25
+ x*=4; // x = x * 4
+ 
+ 
+
+ console.log(x);
+
+ console.log(ageJonas > ageSarah);
+ console.log(ageSarah >= 18);
+ const isFullAge = ageSarah>=18;
+ 
+
+ // coding challange 
+ // creat a boolean variable 'markheigher BMI containing 
+ //information about whether Mark has a heigher BMI
+ const massMark = 78;
+ const heightMark = 1.69;
+ const massJohn = 92;
+ const heightJohn = 1.95;
+
+ const BMIMark = massMark/heightMark**2;
+ const BMIJohn = massJohn/( heightJohn * heightJohn);
+ const markHigherBMI=  BMIMark > BMIJohn;
+ console.log(BMIMark, BMIJohn, markHigherBMI);
+
+ const firstName = 'Jonas';
+ const job = 'Teacher';
+ const birthyear = 1991;
+ const year = 2037;
+ const jonas = "I'm "   + firstName + ', a ' + (year - birthyear)  + ' years old '+ job +'!';
+
+console.log(jonas);
+
+const jonasNew = ` I'm ${firstName}, a ${year - birthyear} year old  ${job}!`;
+
+ console.log(jonasNew);
+  console.log('String with \n\
+  multiple \n\
+  lines');
+ console. log (`String
+ multiple
+ lines`);
+
+
+ //const isOldEnough = age>= 18;
+// if (isOldEnough)
+const age = 15;
+   
+  if (age>= 18){
+  console.log('Sarah can start driving liscense');
+} else
+ {
+   const yearLeft = 18- age;
+   console.log(`Sarah is too young.wait another ${yearLeft} years:)`);
+ }
+
+ const birthYear = 2012;
+
+ let century;
+ if (birthYear <= 2000); {
+   century = 20;
+ } else { 
+   century = 21;
+  }
+
+   console.log(century);*/
+
+   //type of conversion
+
+   
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear)+ 18);
+console.log(Number('santosh'));
+console.log(typeof NaN);
+console.log(String(23), 24);
+
+//type of coercion
+
+console.log('I am ' + '23 ' + 'years old')
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+ 
